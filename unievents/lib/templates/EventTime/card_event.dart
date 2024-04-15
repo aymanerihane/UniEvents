57229e9c73c46f0bbd52f4447a4ca582a2057d28
akram-anissi <148265887+akram-anissi@users.noreply.github.com>
2024-04-15 21:48:01 +0100

    fix

--- unievents/lib/templates/EventTime/card_event.dart
+++ unievents/lib/templates/EventTime/card_event.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:unievents/DB%20&%20Controllers/database_helper.dart';
 import 'package:unievents/DB%20&%20Controllers/userController.dart';
 
-
 class Card_Event extends StatelessWidget {
   final String label;
   final String discription;
@@ -11,15 +10,21 @@ class Card_Event extends StatelessWidget {
   final Color colore;
   final int? eventID;
   final Function()? onTap;
-  const Card_Event({super.key, required this.label, required this.onTap, required this.discription, required this.type, required this.date, required this.colore, this.eventID});
-
-  
+  const Card_Event(
+      {super.key,
+      required this.label,
+      required this.onTap,
+      required this.discription,
+      required this.type,
+      required this.date,
+      required this.colore,
+      this.eventID});
 
   @override
   Widget build(BuildContext context) {
     var currentUser = UserController().currentUser;
     var width = MediaQuery.of(context).size.width;
-    final db= DatabaseHelper();
+    final db = DatabaseHelper();
     const pi = 3.1416;
     return Container(
       margin: const EdgeInsets.only(top: 20, left: 10),
@@ -28,7 +33,7 @@ class Card_Event extends StatelessWidget {
         child: GestureDetector(
           onTap: onTap,
           child: Container(
-            width: width*0.9,
+            width: width * 0.9,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
               color: colore,
@@ -38,7 +43,7 @@ class Card_Event extends StatelessWidget {
               child: Row(
                 children: [
                   Container(
-                    width: width*0.66,
+                    width: width * 0.65,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -51,19 +56,16 @@ class Card_Event extends StatelessWidget {
                             ),
                           ),
                         ),
-                        Row(
-                          children:[ 
-                            Icon(Icons.timelapse_rounded),
-                            Text(
-                              date,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
-                              ),
+                        Row(children: [
+                          Icon(Icons.timelapse_rounded),
+                          Text(
+                            date,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
                             ),
-                          ]
-                        ),
-                    
+                          ),
+                        ]),
                         Container(
                           margin: const EdgeInsets.all(8),
                           child: Text(
@@ -77,13 +79,16 @@ class Card_Event extends StatelessWidget {
                       ],
                     ),
                   ),
-                  Visibility(child:
-                   GestureDetector(
-                    onTap: () {db.deleteEvent(eventID!);},
-                    child: const Icon(Icons.delete)
+                  Visibility(
+                      child: GestureDetector(
+                          onTap: () {
+                            db.deleteEvent(eventID!);
+                          },
+                          child: const Icon(Icons.delete)),
+                      visible: currentUser.usrType == 0),
+                  SizedBox(
+                    width: 11,
                   ),
-                  visible:currentUser.usrType== 0),
-                  SizedBox(width: 11,),
                   const VerticalDivider(
                     color: Colors.white,
                     thickness: 1,
@@ -92,21 +97,23 @@ class Card_Event extends StatelessWidget {
                     indent: 20,
                   ),
                   Container(
-                      child: Transform.rotate(
-                        angle: -90 * pi / 180,
-                        child:Text(type,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,),
+                    child: Transform.rotate(
+                      angle: -90 * pi / 180,
+                      child: Text(
+                        type,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
                         ),
                       ),
+                    ),
                   )
                 ],
               ),
             ),
           ),
         ),
-            ),
-      );
+      ),
+    );
   }
 }
